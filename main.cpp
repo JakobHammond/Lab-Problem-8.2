3b96b73b4f5d2a21317b8d8d3fe1daa9511e1011
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-19 14:13:57 -0400

    completed countWords function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,26 @@
 #include <iostream>
 using namespace std;
 
+int countWords(string str);
+int countCharacters(string str);
 int main(void){
     
+    
+    
+}
+
+int countCharacter(string str){
+    return str.length();
+}
+
+int countWords(string str){
+    int words = 1;
+    for(int i = 1; i <= countCharacter(str); i++){
+        string first;
+        first = str.substr(countCharacter(str) - i);
+        if(first.front() == ' '){
+            words++;
+        }
+    }
+    return words;
 }
